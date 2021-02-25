d835c22e75c647e26df655cb8113b9477cd605ae
Andrei Petrov <andrei.petrov@etu.u-pec.fr>
2021-02-25 10:40:24 +0100

    --> ajout des dependances reactivemongo et du crud

--- gridtp1/app/controllers/OrderController.scala
+++ gridtp1/app/controllers/OrderController.scala
@@ -1,13 +1,17 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import kafka.KafkaProducerImpl
+import models.Item
+import mongo.MongoRepository
+import org.apache.kafka.clients.producer.KafkaProducer
 import play.api.db
 import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import reactivemongo.play.json.collection.JSONCollection
 
 @Singleton
-class OrderController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class OrderController @Inject()(cc: ControllerComponents, mongoRepository: MongoRepository,kafkaproducer: KafkaProducerImpl) extends AbstractController(cc) {
 
   private def asJson(v: Option[JsObject]) = v.map(Ok(_)).getOrElse(NotFound)
 
@@ -16,9 +20,9 @@ class OrderController @Inject()(cc: ControllerComponents) extends AbstractContro
     Ok(views.html.index("grid tp1"))
   }
 
-  def addItemToStock(itemId: Long) = Action {
-
-    Ok(Json.toJson(itemId))
+  def addItemToStock(item: Item) = Action {
+      mongoRepository.insert(item)
+      kafkaproducer.send(item.toString)
   }
 
 }
--- gridtp1/app/kafka/KafkaProducerImpl.scala
+++ gridtp1/app/kafka/KafkaProducerImpl.scala
@@ -5,7 +5,7 @@ import java.util.{Date, Properties}
 import scala.util.Random
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
-object KafkaProducerImpl extends App {
+class KafkaProducerImpl extends App {
   val events = args(0).toInt
   val topic = args(1)
   val brokers = args(2)
@@ -18,20 +18,15 @@ object KafkaProducerImpl extends App {
 
 
   val producer = new KafkaProducer[String, String](props)
-  val t = System.currentTimeMillis()
-  for (nEvents <- Range(0, events)) {
-    val runtime = new Date().getTime()
-    val ip = "192.168.2." + rnd.nextInt(255)
-    val msg = runtime + "," + nEvents + ",www.example.com," + ip
-    val data = new ProducerRecord[String, String](topic, ip, msg)
 
-    //async
-    //producer.send(data, (m,e) => {})
-    //sync
+  def send(item:String): Unit ={
+    val msg = item;
+    val data = new ProducerRecord[String,String](topic,msg)
     producer.send(data)
+    System.out.println("sent data: " + data)
   }
 
-  System.out.println("sent per second: " + events * 1000 / (System.currentTimeMillis() - t))
+
   producer.close()
 
 }
